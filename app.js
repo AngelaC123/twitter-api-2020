91dd3aa01195966499513bfdc951a873a63a0825
AngelaC123 <angelarare@gmail.com>
2022-06-12 16:07:45 +0800

    refactor app.js

--- app.js
+++ app.js
@@ -10,25 +10,15 @@ const port = process.env.PORT || 3000
 
 const routes = require('./routes')
 const passport = require('./config/passport')
-
-const methodOverride = require('method-override')
 const cors = require('cors')
 
-// Setting Cors
 app.use(cors())
-
-// Setting body-parser
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-// Setting passport
 app.use(passport.initialize())
 app.use(passport.session())
 
-// Setting middleware
-app.use(methodOverride('_method'))
-
-// Setting upload path
 app.use('/upload', express.static(path.join(__dirname, 'upload')))
 
 app.use('/api', routes)
